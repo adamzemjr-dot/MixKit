86b3c3d466e213bf4900431a1af0731ea808eada
adamzemjr-dot <adamzemjr@gmail.com>
2026-01-29 11:24:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function goTo(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
-  });
-}
+const button = document.getElementById("cta-btn");
+
+button.addEventListener("click", () => {
+  alert("MixKit coming soon! 🚀");
+});
