132ca2a7c73474397d7852cba4316c008a13525f
adamzemjr-dot <adamzemjr@gmail.com>
2026-02-02 21:01:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
-    document.querySelector(link.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
   });
 });
