7f40f2ce8ef77ce194da9005d8535bda1153cee3
adamzemjr-dot <adamzemjr@gmail.com>
2026-02-02 18:25:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-  card.addEventListener("mouseenter", () => {
-    const liquid = card.querySelector(".liquid");
-    liquid.style.height = "88%";
+function goTo(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
   });
-
-  card.addEventListener("mouseleave", () => {
-    const liquid = card.querySelector(".liquid");
-    liquid.style.height = "0%";
-  });
-});
+}
